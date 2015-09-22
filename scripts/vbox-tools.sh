6d371495624e5ad76a1a2571fd01430919ba35c4
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2015-09-22 14:37:50 +0200

    improve 'vbox-tools.sh' script

--- scripts/vbox-tools.sh
+++ scripts/vbox-tools.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 # Install dependencies
-apt-get update && apt-get install -y gcc make perl linux-headers-$(uname -r)
+apt-get update && apt-get install -y gcc make linux-headers-$(uname -r)
 
 # Mount Guest Additions ISO file
-mount -t iso9660 -o loop /root/VBoxGuestAdditions.iso /mnt
+mkdir /mnt/vbox
+mount -t iso9660 -o loop /root/VBoxGuestAdditions.iso /mnt/vbox
 
 # Execute the installer
-/mnt/VBoxLinuxAdditions.run
+/mnt/vbox/VBoxLinuxAdditions.run
 
 # Display installation logs before deleting them
 cat /var/log/vboxadd-install.log
@@ -15,7 +16,8 @@ rm -f /var/log/vboxadd-install*.log
 rm -f /var/log/VBoxGuestAdditions.log
 
 # Unmount ISO file
-umount /mnt
+umount /mnt/vbox
+rmdir /mnt/vbox
 
 # Delete ISO file
 rm -f /root/VBoxGuestAdditions.iso
