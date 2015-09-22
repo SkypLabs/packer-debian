34c0faba9cca568d9a93328dd110bd0704dc9957
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2015-09-22 14:08:47 +0200

    improve 'vmware-tools.sh' script

--- scripts/vmware-tools.sh
+++ scripts/vmware-tools.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
+# Install dependencies
+apt-get update && apt-get install -y gcc make binutils linux-headers-$(uname -r)
+
 # Mount VMware Tools ISO file
-mount -t iso9660 -o loop /root/linux.iso /mnt
+mkdir /mnt/vmware
+mount -t iso9660 -o loop /root/linux.iso /mnt/vmware
 
 # Execute the installer
 cd /tmp
-cp /mnt/VMwareTools-*.gz .
+cp /mnt/vmware/VMwareTools-*.gz .
 tar zxvf VMwareTools-*.gz
 ./vmware-tools-distrib/vmware-install.pl -d
 
 # Unmount ISO file
-umount /mnt
+umount /mnt/vmware
+rmdir /mnt/vmware
 
 # Delete ISO file
 rm -f /root/linux.iso
