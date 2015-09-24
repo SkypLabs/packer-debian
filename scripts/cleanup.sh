cb4e67e7a55c61a8372370da7a5bb2489400d946
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2015-09-24 10:54:53 +0200

    improve 'cleanup.sh' script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -12,11 +12,15 @@ apt-get -y clean
 
 # Force the logs to rotate and remove old logs we don’t need
 logrotate -f /etc/logrotate.conf
+rm -f /var/log/*.1
 
 # Truncate the audit logs
 cat /dev/null > /var/log/wtmp
 cat /dev/null > /var/log/lastlog
 cat /dev/null > /var/log/grubby
+
+# Remove the udev persistent device rules
+rm -f /etc/udev/rules.d/70-persistent-*.rules
 
 # Clean /tmp out
 rm -rf /tmp/*
@@ -28,3 +32,6 @@ rm -rf ~root/.ssh/
 # Remove the root user’s shell history
 rm -f ~root/.bash_history
 unset HISTFILE
+
+# Remove installation logs
+rm -rf /var/log/installer
